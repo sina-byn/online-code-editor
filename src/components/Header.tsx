7fcacbcd63a5574353aa7c22515325a508d7e228
Sina Bayandorian <sina.sina.bayan@protonmail.com>
2022-11-13 18:36:11 +0330

    header padding fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,13 +12,13 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ children }) => {
   return (
-    <header className='flex justify-between bg-zinc-900 px-2 py-3'>
+    <header className='flex justify-between bg-zinc-900 px-4 py-3'>
       <div className='options left flex items-center gap-x-4'>
         <ThemeSelect />
         <ModeSwitch />
         {children}
       </div>
-      <div className='options right flex items-center pr-4'>
+      <div className='options right flex items-center'>
         <FullscreenButton className='text-xl' />
       </div>
     </header>
